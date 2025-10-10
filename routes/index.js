69812a2d93e297cc6800f6607dc90f14ca1bc7c2
jujunsetiawan <jujunsetiawan10@gmail.com>
2025-10-10 16:10:33 +0700

    feat: add kubernetes config

--- routes/index.js
+++ routes/index.js
@@ -8,9 +8,12 @@ router.get('/', (req, res, next) => {
     name: "The_Wisdom_API", 
     version: "0.0.1", 
     docs_link: "https://documenter.getpostman.com/view/14970426/2sB3QFQXp2", 
-    health_check: 100, 
+    health_check: 100,
     is_open: true
   });
 });
+
+router.get('/_status/readyz', (req, res) => res.status(200).send('ready'));
+router.get('/_status/healthz', (req, res) => res.status(200).send('ok'));
 
 module.exports = router;
